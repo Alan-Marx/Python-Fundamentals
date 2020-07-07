6bac0a73b90f19b611d45e83a1df1cf4f517382a
Alan-Marx <alanmarx@hotmail.com>
2020-07-06 22:05:51 -0700

    renamed file, added aditional notes and examples

---
+++ basics.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+lbs = float(input("Please enter the lbs of water treated: "))
+ounces = lbs * 16
+gallons = int(ounces / 128)
+ounces = ounces - gallons * 128
+print("That’s",gallons,"gallons and", \
+   ounces,"ounces of treated waste water.")
+
+# help(type) will output documentation for a particular Python3 data type 
