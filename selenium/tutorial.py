6cb507cd964c9ed6960d113a22993faab5f283b5
Alan-Marx <alanmarx@hotmail.com>
2020-07-07 11:31:11 -0700

    installed selenium and chromedriver, opened up a website with selenium webdriver

---
+++ selenium/tutorial.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+PATH = "/Users/alanmarx/Program Files/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+# running this file will open up the provided website: 
+driver.get("https://techwithtim.net")
