3d71ce83a27611c6c20a8d73f960ae7370c62866
Alan-Marx <alanmarx@hotmail.com>
2020-07-07 10:04:59 -0700

    defined a test function as well as an entry point conditional that calls the function

--- testing/test_sum.py
+++ testing/test_sum.py
@@ -1,4 +1,14 @@
 # assert is a built-in keyword in Python
+# there will only be output if there is an assertion error
 
-assert sum([1, 2, 3]) == 6, "Should be 6"
-assert sum([1, 1, 1]) == 6, "Should be 6"
+# this is how you define a function in python:
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+    assert sum([1, 1, 1]) == 6, "Should be 6"
+
+# this is how you define an "if" statement. The following conditional defines the files' entry point, meaning that when you run the file on the command line it will call the functions inside the statement:
+if __name__ == "__main__":
+    # you call a function just like in JS
+    test_sum() 
+    # the following function wont be called given that an assertion error will be detected as a result of the previous function call
+    print("Everything passed")
