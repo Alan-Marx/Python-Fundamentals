9ef69318c9504c4a1b61302c8930f3ad68d6b323
Alan-Marx <alanmarx@hotmail.com>
2020-07-07 12:06:49 -0700

    expanded tutorial.py to not only open a webpage but also find the search bar and input text into it

--- selenium/tutorial.py
+++ selenium/tutorial.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+# the following import will give us access to functionality such as the enter key, the escape key, etc.
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 PATH = "/Users/alanmarx/Program Files/chromedriver"
 driver = webdriver.Chrome(PATH)
@@ -6,4 +10,15 @@ driver = webdriver.Chrome(PATH)
 # running this file will open up the provided website: 
 driver.get("https://techwithtim.net")
 print(driver.title)
+
+# will find the first HTML element on the page with the name "s"
+search = driver.find_element_by_name("s")
+# will input the string "test" on the HTML element located above
+search.send_keys("test")
+# will hit the enter/return key after we have inputted the string
+search.send_keys(Keys.RETURN)
+
+# pauses the program for 5 seconds:
+time.sleep(5)
+
 driver.quit()
